710791cdaa2172d6f67e5fc375929c7da863c681
huy <huy@huyng.com>
2016-01-15 08:23:32 -0800

    updated example

--- vgg.py
+++ vgg.py
@@ -1,3 +1,5 @@
+# Adapted from https://tensorflow.googlesource.com/tensorflow/+/master/tensorflow/models/image/cifar10/cifar10.py
+
 from datetime import datetime
 import math
 import time
@@ -6,48 +8,28 @@ import tensorflow.python.platform
 import tensorflow as tf
 
 
-
 batch_size = 2
-parameters = []
-conv_counter = 1
-pool_counter = 1
-affine_counter = 1
 
-
-def layer_conv2d(inputs, n_in, n_out, kh, kw, dh, dw, padding):
-    global conv_counter
-    global parameters
-    name = 'conv' + str(conv_counter)
-    conv_counter += 1
+def conv_op(input_op, name, kw, kh, n_in, n_out, dw, dh):
     with tf.name_scope(name) as scope:
-        kernel = tf.Variable(tf.truncated_normal([kh, kw, n_in, n_out], dtype=tf.float32, stddev=1e-1), name='weights')
-        conv = tf.nn.conv2d(inputs, kernel, [1, dh, dw, 1], padding=padding)
-        biases = tf.Variable(tf.constant(0.0, shape=[n_out], dtype=tf.float32), trainable=True, name='biases')
-        bias = tf.reshape(tf.nn.bias_add(conv, biases), conv.get_shape())
-        conv1 = tf.nn.relu(bias, name=scope)
-        parameters += [kernel, biases]
-        print conv1.name
-        print biases.name
-        return conv1
+        kernel_init_val = tf.truncated_normal([kh, kw, n_in, n_out], dtype=tf.float32, stddev=1e-1)
+        kernel = tf.Variable(kernel_init_val, trainable=True, name='w')
+        conv = tf.nn.conv2d(input_op, kernel, (1, dh, dw, 1), padding='SAME')
+        bias_init_val = tf.constant(0.0, shape=[n_out], dtype=tf.float32)
+        biases = tf.Variable(bias_init_val, trainable=True, name='b')
+        z = tf.reshape(tf.nn.bias_add(conv, biases), conv.get_shape())
+        activation = tf.nn.relu(z, name=scope)
+        return activation
 
-def layer_affine(inputs, n_in, n_out):
-    global affine_counter
-    global parameters
-    name = 'affine' + str(affine_counter)
-    affine_counter += 1
+def affine_op(input_op, name, n_in, n_out):
     with tf.name_scope(name) as scope:
-        kernel = tf.Variable(tf.truncated_normal([n_in, n_out], dtype=tf.float32, stddev=1e-1), name='weights')
+        kernel = tf.Variable(tf.truncated_normal([n_in, n_out], dtype=tf.float32, stddev=1e-1),  trainable=True, name='weights')
         biases = tf.Variable(tf.constant(0.0, shape=[n_out], dtype=tf.float32), trainable=True, name='biases')
-        affine1 = tf.nn.relu_layer(inputs, kernel, biases, name=name)
-        parameters += [kernel, biases]
+        affine1 = tf.nn.relu_layer(input_op, kernel, biases, name=name)
         return affine1
 
-def layer_maxpool(inputs, kh, kw, dh, dw):
-    global pool_counter
-    global parameters
-    name = 'pool' + str(pool_counter)
-    pool_counter += 1
-    return tf.nn.max_pool(inputs,
+def mpool_op(input_op, name, kh, kw, dh, dw):
+    return tf.nn.max_pool(input_op,
                           ksize=[1, kh, kw, 1],
                           strides=[1, dh, dw, 1],
                           padding='VALID',
@@ -63,10 +45,35 @@ def loss(logits, labels):
     return loss
 
 
+def inference(input_op):
+    conv1 = conv_op(input_op, name="conv1", kh=3, kw=3, n_in=3, n_out=64, dh=1, dw=1)
+    pool1 = mpool_op(conv1,   name="pool1", kh=2, kw=2, dw=2, dh=2)
+    conv2 = conv_op(pool1,    name="conv2", kh=3, kw=3, n_in=64, n_out=128, dh=1, dw=1)
+    pool2 = mpool_op(conv2,   name="pool2", kh=2, kw=2, dh=2, dw=2)
+    conv3 = conv_op(pool2,    name="conv3", kh=3, kw=3, n_in=128, n_out=512, dh=1, dw=1)
+    conv4 = conv_op(conv3,    name="conv4", kh=3, kw=3, n_in=512, n_out=512, dh=1, dw=1)
+    pool4 = mpool_op(conv4,   name="pool4", kh=2, kw=2, dh=2, dw=2)
+    conv5 = conv_op(pool4,    name="conv5", kh=3, kw=3, n_in=512, n_out=512, dh=1, dw=1)
+    conv6 = conv_op(conv5,    name="conv6", kh=3, kw=3, n_in=512, n_out=512, dh=1, dw=1)
+    pool6 = mpool_op(conv6,   name="pool6", kh=2, kw=2, dh=2, dw=2)
+    conv7 = conv_op(pool6,    name="conv7", kh=3, kw=3, n_in=512, n_out=512, dh=1, dw=1)
+    conv8 = conv_op(conv7,    name="conv8", kh=3, kw=3, n_in=512, n_out=512, dh=1, dw=1)
+    pool8 = mpool_op(conv8,   name="pool8", kh=2, kw=2, dw=2, dh=2)
+    resh1 = tf.reshape(pool8, [-1,512*7*7], name="resh1")
+    affn1 = affine_op(resh1,  name="affn1", n_in=512*7*7, n_out=4096)
+    affn2 = affine_op(affn1,  name="affn2", n_in=4096, n_out=4096)
+    affn3 = affine_op(affn2,  name="affn3", n_in=4096, n_out=1000)
+    return affn3
 
+def predict():
+    pass
 
-def run_benchmark():
-    global parameters
+def train(lr=0.01):
+    """
+    train model
+
+    :param lr:  This is the learning rate
+    """
     with tf.Graph().as_default():
         # Generate some dummy images.
         image_size = 224
@@ -80,51 +87,26 @@ def run_benchmark():
 
         # Build a Graph that computes the logits predictions from the
         # inference model.
-        conv1 = layer_conv2d(images, n_in=3, n_out=64, kh=3, kw=3, dh=1, dw=1, padding='SAME')
-        pool1 = layer_maxpool(conv1, kh=2, kw=2, dh=2, dw=2)
-        conv2 = layer_conv2d(pool1,  n_in=64, n_out=128, kh=3, kw=3, dh=1, dw=1, padding='SAME')
-        pool2 = layer_maxpool(conv2, kh=2, kw=2, dh=2, dw=2)
-        conv3 = layer_conv2d(pool2,  n_in=128, n_out=256, kh=3, kw=3, dh=1, dw=1, padding='SAME')
-        conv4 = layer_conv2d(conv3,  n_in=256, n_out=256, kh=3, kw=3, dh=1, dw=1, padding='SAME')
-        pool4 = layer_maxpool(conv4, kh=2, kw=2, dh=2, dw=2)
-        conv5 = layer_conv2d(pool4,  n_in=256, n_out=512, kh=3, kw=3, dh=1, dw=1, padding='SAME')
-        conv6 = layer_conv2d(conv5,  n_in=512, n_out=512, kh=3, kw=3, dh=1, dw=1, padding='SAME')
-        pool6 = layer_maxpool(conv6, kh=2, kw=2, dh=2, dw=2)
-        conv7 = layer_conv2d(pool6,  n_in=512, n_out=512, kh=3, kw=3, dh=1, dw=1, padding='SAME')
-        conv8 = layer_conv2d(conv7,  n_in=512, n_out=512, kh=3, kw=3, dh=1, dw=1, padding='SAME')
-        pool8 = layer_maxpool(conv8, kh=2, kw=2, dw=2, dh=2)
-        resh1 = tf.reshape(pool8,    [-1, 512*7*7])
-        affn1 = layer_affine(resh1,  n_in=512*7*7, n_out=4096)
-        affn2 = layer_affine(affn1,  n_in=4096, n_out=4096)
-        affn3 = layer_affine(affn2,  n_in=4096, n_out=1000)
+        last_layer = inference(images)
+
 
         # Build an initialization operation.
-        init = tf.initialize_all_variables()
+        initializer = tf.initialize_all_variables()
 
         # Start running operations on the Graph.
         sess = tf.Session()
-        sess.run(init)
-
-
-        # Run the forward benchmark.
-        sess.run(affn3)
+        sess.run(initializer)
 
         # Add a simple objective so we can calculate the backward pass.
-        objective = loss(affn3, labels)
-        # Compute the gradient with respect to all the parameters.
-        # grad = tf.gradients(objective, parameters)
-        train_step = tf.train.GradientDescentOptimizer(0.01).minimize(objective)
+        objective = loss(last_layer, labels)
+        train_step = tf.train.GradientDescentOptimizer(lr).minimize(objective)
         for i in range(1000):
             print i
             sess.run(train_step)
-
-        # Run the backward benchmark.
-        # sess.run(grad)
+        sess.close()
 
 
-def main(_):
-  run_benchmark()
 
 
 if __name__ == '__main__':
-  tf.app.run()
+    train()
