4fde6c29711bfc0c89f89f0b0efd20d25ec4a693
huy <huy@huyng.com>
2016-01-22 08:37:53 -0800

    added prediction function

--- vgg.py
+++ vgg.py
@@ -9,11 +9,13 @@ import tensorflow.python.platform
 import tensorflow as tf
 
 
-batch_size = 10
+batch_size = 8
 
-def conv_op(input_op, name, kw, kh, n_in, n_out, dw, dh):
+def conv_op(input_op, name, kw, kh, n_out, dw, dh):
+    n_in = input_op.get_shape()[-1].value
+
     with tf.name_scope(name) as scope:
-        kernel_init_val = tf.truncated_normal([kh, kw, n_in, n_out], dtype=tf.float32, stddev=1e-1)
+        kernel_init_val = tf.truncated_normal([kh, kw, n_in, n_out], dtype=tf.float32, stddev=0.1)
         kernel = tf.Variable(kernel_init_val, trainable=True, name='w')
         conv = tf.nn.conv2d(input_op, kernel, (1, dh, dw, 1), padding='SAME')
         bias_init_val = tf.constant(0.0, shape=[n_out], dtype=tf.float32)
@@ -22,10 +24,12 @@ def conv_op(input_op, name, kw, kh, n_in, n_out, dw, dh):
         activation = tf.nn.relu(z, name=scope)
         return activation
 
-def fc_op(input_op, name, n_in, n_out):
+def fc_op(input_op, name, n_out):
+    n_in = input_op.get_shape()[-1].value
+
     with tf.name_scope(name) as scope:
-        kernel = tf.Variable(tf.truncated_normal([n_in, n_out], dtype=tf.float32, stddev=1e-1), trainable=True, name='weights')
-        biases = tf.Variable(tf.constant(0.0, shape=[n_out], dtype=tf.float32), trainable=True, name='biases')
+        kernel = tf.Variable(tf.truncated_normal([n_in, n_out], dtype=tf.float32, stddev=0.1), name='w')
+        biases = tf.Variable(tf.constant(0.0, shape=[n_out], dtype=tf.float32), name='b')
         activation = tf.nn.relu_layer(input_op, kernel, biases, name=name)
         return activation
 
@@ -35,6 +39,7 @@ def mpool_op(input_op, name, kh, kw, dh, dw):
                           strides=[1, dh, dw, 1],
                           padding='VALID',
                           name=name)
+
 
 def loss(logits, labels):
     labels = tf.expand_dims(labels, 1)
@@ -46,49 +51,50 @@ def loss(logits, labels):
     return loss
 
 
-def inference_vgg(input_op, dropout_keep_prob, input_shape=224):
+def inference_vgg(input_op, dropout_keep_prob):
 
     # assume input_op shape is 224x224x3
 
     # block 1 -- outputs 112x112x64
-    conv1_1 = conv_op(input_op, name="conv1_1", kh=3, kw=3, n_in=3,  n_out=64, dh=1, dw=1)
-    conv1_2 = conv_op(conv1_1,  name="conv1_2", kh=3, kw=3, n_in=64, n_out=64, dh=1, dw=1)
-    pool1 = mpool_op(conv1_2,   name="pool1",   kh=2, kw=2, dw=2, dh=2)
+    conv1_1 = conv_op(input_op, name="conv1_1", kh=3, kw=3, n_out=64, dh=1, dw=1)
+    conv1_2 = conv_op(conv1_1,  name="conv1_2", kh=3, kw=3, n_out=64, dh=1, dw=1)
+    pool1 = mpool_op(conv1_2,   name="pool1",   kh=4, kw=4, dw=4, dh=4)
 
     # block 2 -- outputs 56x56x128
-    conv2_1 = conv_op(pool1,    name="conv2_1", kh=3, kw=3, n_in=64,  n_out=128, dh=1, dw=1)
-    conv2_2 = conv_op(conv2_1,  name="conv2_2", kh=3, kw=3, n_in=128, n_out=128, dh=1, dw=1)
-    pool2 = mpool_op(conv2_2,   name="pool2",   kh=2, kw=2, dh=2, dw=2)
+    conv2_1 = conv_op(pool1,    name="conv2_1", kh=3, kw=3, n_out=128, dh=1, dw=1)
+    conv2_2 = conv_op(conv2_1,  name="conv2_2", kh=3, kw=3, n_out=128, dh=1, dw=1)
+    pool2 = mpool_op(conv2_2,   name="pool2",   kh=4, kw=4, dh=4, dw=4)
 
-    # block 3 -- outputs 28x28x256
-    conv3_1 = conv_op(pool2,    name="conv3_1", kh=3, kw=3, n_in=128, n_out=256, dh=1, dw=1)
-    conv3_2 = conv_op(conv3_1,  name="conv3_2", kh=3, kw=3, n_in=256, n_out=256, dh=1, dw=1)
-    pool3 = mpool_op(conv3_2,   name="pool3",   kh=2, kw=2, dh=2, dw=2)
-
-    # block 4 -- outputs 14x14x512
-    conv4_1 = conv_op(pool3,    name="conv4_1", kh=3, kw=3, n_in=256, n_out=512, dh=1, dw=1)
-    conv4_2 = conv_op(conv4_1,  name="conv4_2", kh=3, kw=3, n_in=512, n_out=512, dh=1, dw=1)
-    conv4_3 = conv_op(conv4_2,  name="conv4_2", kh=3, kw=3, n_in=512, n_out=512, dh=1, dw=1)
-    pool4 = mpool_op(conv4_3,   name="pool4",   kh=2, kw=2, dh=2, dw=2)
-
-    # block 5 -- outputs 7x7x512
-    conv5_1 = conv_op(pool4,    name="conv5_1", kh=3, kw=3, n_in=512, n_out=512, dh=1, dw=1)
-    conv5_2 = conv_op(conv5_1,  name="conv5_2", kh=3, kw=3, n_in=512, n_out=512, dh=1, dw=1)
-    conv5_3 = conv_op(conv5_2,  name="conv5_3", kh=3, kw=3, n_in=512, n_out=512, dh=1, dw=1)
-    pool5 = mpool_op(conv5_3,   name="pool5",   kh=2, kw=2, dw=2, dh=2)
+    # # block 3 -- outputs 28x28x256
+    # conv3_1 = conv_op(pool2,    name="conv3_1", kh=3, kw=3, n_out=256, dh=1, dw=1)
+    # conv3_2 = conv_op(conv3_1,  name="conv3_2", kh=3, kw=3, n_out=256, dh=1, dw=1)
+    # pool3 = mpool_op(conv3_2,   name="pool3",   kh=2, kw=2, dh=2, dw=2)
+    #
+    # # block 4 -- outputs 14x14x512
+    # conv4_1 = conv_op(pool3,    name="conv4_1", kh=3, kw=3, n_out=512, dh=1, dw=1)
+    # conv4_2 = conv_op(conv4_1,  name="conv4_2", kh=3, kw=3, n_out=512, dh=1, dw=1)
+    # conv4_3 = conv_op(conv4_2,  name="conv4_2", kh=3, kw=3, n_out=512, dh=1, dw=1)
+    # pool4 = mpool_op(conv4_3,   name="pool4",   kh=2, kw=2, dh=2, dw=2)
+    #
+    # # block 5 -- outputs 7x7x512
+    # conv5_1 = conv_op(pool4,    name="conv5_1", kh=3, kw=3, n_out=512, dh=1, dw=1)
+    # conv5_2 = conv_op(conv5_1,  name="conv5_2", kh=3, kw=3, n_out=512, dh=1, dw=1)
+    # conv5_3 = conv_op(conv5_2,  name="conv5_3", kh=3, kw=3, n_out=512, dh=1, dw=1)
+    # pool5 = mpool_op(conv5_3,   name="pool5",   kh=2, kw=2, dw=2, dh=2)
 
     # flatten
-    flattened_shape = (input_shape/(2**5))**2 * 512 # 5 here comes from the number of pooling layers
-    resh1 = tf.reshape(pool5, [-1, flattened_shape], name="resh1")
+    shp = pool2.get_shape()
+    flattened_shape = shp[1].value * shp[2].value * shp[3].value
+    resh1 = tf.reshape(pool2, [-1, flattened_shape], name="resh1")
 
     # fully connected
-    fc6 = fc_op(resh1, name="fc6", n_in=flattened_shape, n_out=4096)
+    fc6 = fc_op(resh1, name="fc6", n_out=4096)
     fc6_drop = tf.nn.dropout(fc6, dropout_keep_prob, name="fc6_drop")
 
-    fc7 = fc_op(fc6_drop, name="fc7", n_in=4096, n_out=10)
+    fc7 = fc_op(fc6_drop, name="fc7", n_out=10)
     fc7_drop = tf.nn.dropout(fc7, dropout_keep_prob, name="fc7_drop")
 
-    fc8 = fc_op(fc7_drop, name="fc8", n_in=10, n_out=10)
+    fc8 = fc_op(fc7_drop, name="fc8", n_out=10)
     return fc8
 
 
@@ -146,12 +152,12 @@ def train(lr=0.0001, max_step=5000*10):
         # Build a Graph that computes the logits predictions from the
         # inference model.
         # last_layer = inference_vgg(images, dropout_keep_prob)
-        last_layer = inference_vgg(images, dropout_keep_prob, input_shape=64)
+        last_layer = inference_vgg(images, dropout_keep_prob )
 
         # Add a simple objective so we can calculate the backward pass.
         objective = loss(last_layer, labels)
-        accuracy, total_correct = evaluate(last_layer, labels)
-        optimizer = tf.train.AdagradOptimizer(lr)
+        _, total_correct = evaluate(last_layer, labels)
+        optimizer = tf.train.RMSPropOptimizer(lr, 0.9)
         global_step = tf.Variable(0, name="global_step", trainable=False)
         train_step = optimizer.minimize(objective, global_step=global_step)
 
@@ -187,11 +193,11 @@ def train(lr=0.0001, max_step=5000*10):
 
                 t0 = time.time()
                 result = sess.run(
-                    [train_step, objective, summary_op, accuracy, maintain_averages_op],
+                    [train_step, objective, summary_op, maintain_averages_op],
                     feed_dict = {
                         in_images: X,
                         labels: Y,
-                        dropout_keep_prob: 1.0
+                        dropout_keep_prob: 0.5
                     }
                 )
                 duration = time.time() - t0
@@ -222,8 +228,11 @@ def train(lr=0.0001, max_step=5000*10):
                             labels: Y_tst,
                             dropout_keep_prob: 1.0
                         })
-                    print("%s tst accuracy = %.3f" % (datetime.now(), float(correct_count)/num_tst_examples))
-
+                    accuracy = float(correct_count)/num_tst_examples
+                    print("%s tst accuracy = %.3f" % (datetime.now(), accuracy))
+                    if accuracy > 0.9:
+                        checkpoint_path = saver.save(sess, "checkpoints/model.ckpt")
+                        print("saving model %s" % checkpoint_path)
 
 
 
